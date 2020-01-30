47d08603fd8346cae060b36b5206ee280a246456
Paul Ssebaale <pssebaale@UG-PC0BZ3MK.clintonhealthaccess.org>
2020-01-30 09:00:53 +0300

    Paste from Private Repo

---
+++ config/db.js
@@ -0,0 +1,21 @@
+//FILENAME : db.js
+
+// Import Mongoose
+let mongoose = require('mongoose');
+
+// Replace this with your MONGOURI.
+const MONGOURI = 'mongodb://localhost/ugopps2';
+
+const InitiateMongoServer = async () => {
+    try {
+    await mongoose.connect(MONGOURI, {
+        useNewUrlParser: true
+    });
+    console.log("Connected to DB !!");
+    } catch (e) {
+    console.log(e);
+    throw e;
+    }
+};
+
+module.exports = InitiateMongoServer;
---
+++ index.js
@@ -0,0 +1,33 @@
+// FileName: index.js
+
+// Import express
+let express = require('express');
+const InitiateMongoServer = require("./config/db");
+
+// Import routes
+const auth = require("./routes/auth"); 
+
+// Initialise the app
+let app = express();
+
+// Configure express to handle post requests
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+// Initiate Mongo Server
+InitiateMongoServer();
+
+// Setup server port
+var port = process.env.PORT || 8080;
+
+// Send message for default URL
+app.get('/', (req, res) => res.send('Welcome to UG-OPS 2 API'));
+
+// Use Api routes in the App
+app.use("/auth", auth);
+
+
+// Launch app to listen to specified port
+app.listen(port, function () {
+    console.log("Running UG-OPS 2 on port " + port);
+});
