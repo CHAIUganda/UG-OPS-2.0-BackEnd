f5a76dde55ea5a9fa7f7ed122af8390cdca8632a
Paul Ssebaale <pssebaale@UG-PC0BZ3MK.clintonhealthaccess.org>
2020-02-18 15:55:52 +0300

    update to return holiday object

--- controller/hr/createPublicHoliday.js
+++ controller/hr/createPublicHoliday.js
@@ -33,8 +33,8 @@ const createPublicHoliday = async (req, res) => {
     });
 
     await holidaytoSave.save();
-    res.status(200).json({
-      message: 'Public Holiday Created successfully'
+    res.status(201).json({
+      holidaytoSave
     });
   } catch (err) {
     debug(err.message);
