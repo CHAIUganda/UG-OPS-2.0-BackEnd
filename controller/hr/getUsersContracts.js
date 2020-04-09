4df99e2e1814938d7b8336515058121f4f6e4d47
Paul Ssebaale <pssebaale@UG-PC0BZ3MK.clintonhealthaccess.org>
2020-04-09 11:19:06 +0300

    prog mgr details

--- controller/hr/getUsersContracts.js
+++ controller/hr/getUsersContracts.js
@@ -56,115 +56,115 @@ const getUsersContracts = async (req, res) => {
           contractId = contract._id;
           contractDismiss = contract.contractDismiss;
           contractSnooze = contract.contractSnooze;
-        }
 
-        const supervisor = await User.findOne({
-          email: arr[controller].supervisorEmail,
-        });
-
-        let supervisorDetails;
-        if (!supervisor) {
-          supervisorDetails = {
-            Supervisor_id: null,
-            fName: null,
-            lName: null,
-            email: null,
-          };
-        } else {
-          supervisorDetails = {
-            _id: supervisor._id,
-            fName: supervisor.fName,
-            lName: supervisor.lName,
-            email: supervisor.email,
-          };
-        }
-        let program;
-        let programShortForm;
-        let programManagerId;
-        let programManagerDetails;
-
-        const userProgram = await Program.findOne({
-          _id: programId,
-        });
-
-        if (!userProgram) {
-          program = 'NA';
-          programShortForm = 'NA';
-          programManagerDetails = {
-            Supervisor_id: null,
-            fName: null,
-            lName: null,
-            email: null,
-          };
-          // eslint-disable-next-line no-else-return
-        } else {
-          programManagerId = userProgram.program;
-          program = userProgram.program;
-          programShortForm = userProgram.shortForm;
-          const userPM = await User.findOne({
-            _id: programManagerId,
+          const supervisor = await User.findOne({
+            email: arr[controller].supervisorEmail,
           });
-          if (!userPM) {
-            programManagerDetails = {
+
+          let supervisorDetails;
+          if (!supervisor) {
+            supervisorDetails = {
               Supervisor_id: null,
               fName: null,
               lName: null,
               email: null,
             };
           } else {
-            programManagerDetails = {
-              _id: user._id,
-              fName: user.fName,
-              lName: user.lName,
-              email: user.email,
+            supervisorDetails = {
+              _id: supervisor._id,
+              fName: supervisor.fName,
+              lName: supervisor.lName,
+              email: supervisor.email,
             };
           }
-        }
+          let program;
+          let programShortForm;
+          let programManagerId;
+          let programManagerDetails;
 
-        let endDate = contract.contractEndDate;
-        // set timezone to kampala
-        const CurrentDate = moment().tz('Africa/Kampala').format();
+          const userProgram = await Program.findOne({
+            _id: programId,
+          });
 
-        endDate = moment(endDate);
-        const diff = endDate.diff(CurrentDate, 'days') + 1;
-        const daysLeftonContract = diff;
-        // eslint-disable-next-line eqeqeq
-        if (diff < expiryIn || diff == expiryIn) {
-          const userRemade = {
-            _id,
-            fName,
-            lName,
-            supervisorDetails,
-            gender,
-            roles,
-            title,
-            birthDate,
-            programId,
-            program,
-            programShortForm,
-            programManagerDetails,
-            oNames,
-            email,
-            type,
-            level,
-            bankAccounts,
-            team,
-            annualLeaveBF,
-            contractId,
-            contractStartDate,
-            contractEndDate,
-            contractType,
-            contractStatus,
-            contractDismiss,
-            contractSnooze,
-            daysLeftonContract,
-          };
+          if (!userProgram) {
+            program = 'NA';
+            programShortForm = 'NA';
+            programManagerDetails = {
+              Supervisor_id: null,
+              fName: null,
+              lName: null,
+              email: null,
+            };
+            // eslint-disable-next-line no-else-return
+          } else {
+            programManagerId = userProgram.program;
+            program = userProgram.program;
+            programShortForm = userProgram.shortForm;
+            const userPM = await User.findOne({
+              _id: programManagerId,
+            });
+            if (!userPM) {
+              programManagerDetails = {
+                Supervisor_id: null,
+                fName: null,
+                lName: null,
+                email: null,
+              };
+            } else {
+              programManagerDetails = {
+                _id: user._id,
+                fName: user.fName,
+                lName: user.lName,
+                email: user.email,
+              };
+            }
+          }
 
-          combinedArray.push(userRemade);
+          let endDate = contract.contractEndDate;
+          // set timezone to kampala
+          const CurrentDate = moment().tz('Africa/Kampala').format();
 
-          recurseProcessLeave(controller + 1, arr);
-        } else {
-          recurseProcessLeave(controller + 1, arr);
+          endDate = moment(endDate);
+          const diff = endDate.diff(CurrentDate, 'days') + 1;
+          const daysLeftonContract = diff;
+          // eslint-disable-next-line eqeqeq
+          if (diff < expiryIn || diff == expiryIn) {
+            const userRemade = {
+              _id,
+              fName,
+              lName,
+              supervisorDetails,
+              gender,
+              roles,
+              title,
+              birthDate,
+              programId,
+              program,
+              programShortForm,
+              programManagerDetails,
+              oNames,
+              email,
+              type,
+              level,
+              bankAccounts,
+              team,
+              annualLeaveBF,
+              contractId,
+              contractStartDate,
+              contractEndDate,
+              contractType,
+              contractStatus,
+              contractDismiss,
+              contractSnooze,
+              daysLeftonContract,
+            };
+
+            combinedArray.push(userRemade);
+
+            recurseProcessLeave(controller + 1, arr);
+          } else {
+            recurseProcessLeave(controller + 1, arr);
+          }
         }
       } else {
         res.status(200).json(combinedArray);
