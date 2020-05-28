cd023c0d60e62bf73ff750f221394776b92ad783
Paul Ssebaale <pssebaale@UG-PC0BZ3MK.clintonhealthaccess.org>
2020-05-28 19:16:52 +0300

    chnged scheduled time

--- helpers/schedule.js
+++ helpers/schedule.js
@@ -17,7 +17,7 @@ log4js.configure({
 // const logger = log4js.getLogger('Timed');
 
 const schedule = new CronJob(
-  '0 02 19 * * *',
+  '0 18 19 * * *',
   () => {
     console.log('Checking for birthdays and Ripe leaves');
     // contains auto scheduled jobs to be done by system
