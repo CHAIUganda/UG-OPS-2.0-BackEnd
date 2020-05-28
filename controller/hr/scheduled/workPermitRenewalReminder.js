6af84fcb7f578f21862590a912c2ac333747d0db
Paul Ssebaale <pssebaale@UG-PC0BZ3MK.clintonhealthaccess.org>
2020-05-28 18:46:06 +0300

    async added on mailer

--- controller/hr/scheduled/workPermitRenewalReminder.js
+++ controller/hr/scheduled/workPermitRenewalReminder.js
@@ -111,7 +111,7 @@ Disclaimer: This is an auto-generated mail. Please do not reply to it.`;
 ${fName} ${lName}'s Work Permit will expiry in ${diff} days as of ${today.toDateString()}. This is a remainder to start its renewal process."${footer}.
                                                     `;
                   const cc = `${programMngr.email},${supervisor.email}`;
-                  Mailer(from, hr.email, subject, textUser, cc);
+                  await Mailer(from, hr.email, subject, textUser, cc);
                   // save notification on user obj
                   const notificationTitle = `${fName} ${lName}'s WorkPermit will expiry in ${diff} days`;
                   const notificationType = '/hr/WorkPermitsExpiry';
@@ -120,15 +120,15 @@ ${fName} ${lName}'s Work Permit will expiry in ${diff} days as of ${today.toDate
                   // prettier-ignore
                   // eslint-disable-next-line max-len
                   const notificationMessage = `${fName} ${lName}'s Work Permit will expiry in ${diff} days, this is a notification to initiate their contract renewal process.`;
-                  // storeNotification(
-                  //   supervisor,
-                  //   notificationTitle,
-                  //   notificationMessage,
-                  //   null,
-                  //   refType,
-                  //   refId
-                  // );
-                  storeNotification(
+                  await storeNotification(
+                    supervisor,
+                    notificationTitle,
+                    notificationMessage,
+                    null,
+                    refType,
+                    refId
+                  );
+                  await storeNotification(
                     hr,
                     notificationTitle,
                     notificationMessage,
@@ -136,14 +136,14 @@ ${fName} ${lName}'s Work Permit will expiry in ${diff} days as of ${today.toDate
                     refType,
                     refId
                   );
-                  // storeNotification(
-                  //   programMngr,
-                  //   notificationTitle,
-                  //   notificationMessage,
-                  //   null,
-                  //   refType,
-                  //   refId
-                  // );
+                  await storeNotification(
+                    programMngr,
+                    notificationTitle,
+                    notificationMessage,
+                    null,
+                    refType,
+                    refId
+                  );
 
                   recurseProcessLeave(controller + 1, arr);
                 } else {
