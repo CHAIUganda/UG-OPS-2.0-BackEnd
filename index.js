8ec7bfa3172c89a112f9a98d67a3d97860adb23f
Paul Ssebaale <pssebaale@UG-PC0BZ3MK.clintonhealthaccess.org>
2020-07-14 11:25:57 +0300

    Https test on Backend

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const debug = require('debug')('server');
 const morgan = require('morgan');
 const fileUpload = require('express-fileupload');
 const cors = require('cors');
+const fs = require('fs');
+const https = require('https');
 const InitiateMongoServer = require('./config/db');
 const schedule = require('./helpers/schedule');
 const scheduleAnually = require('./helpers/scheduleAnually');
@@ -50,10 +52,18 @@ app.use('/leaveApi', authenticationRequired, leaveApi);
 app.use('/hrApi', authenticationRequired, hrApi);
 
 // Launch app to listen to specified port
-app.listen(port, () => {
-  debug(`Running UG-OPS 2 on port ${port}`);
-  console.log(`Running UG-OPS 2 on port ${port}`);
-});
+https
+  .createServer(
+    {
+      key: fs.readFileSync('/certs/privateKey.key'),
+      cert: fs.readFileSync('/certs/certificate.crt'),
+    },
+    app
+  )
+  .listen(port, () => {
+    debug(`Running UG-OPS 2 on port ${port}`);
+    console.log(`Running UG-OPS 2 on port ${port}`);
+  });
 // schedule operations
 schedule.start();
 scheduleAnually.start();
