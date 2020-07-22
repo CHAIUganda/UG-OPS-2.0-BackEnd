7fb15cfcfa397b5506c9e46e14101e3b553961fe
Paul Ssebaale <pssebaale@UG-PC0BZ3MK.clintonhealthaccess.org>
2020-07-22 17:09:08 +0300

    okta

--- middleware/oktaAuthenticator.js
+++ middleware/oktaAuthenticator.js
@@ -2,7 +2,7 @@ const OktaJwtVerifier = require('@okta/jwt-verifier');
 
 const oktaJwtVerifier = new OktaJwtVerifier({
   clientId: process.env.CLIENT_ID,
-  issuer: `https://${process.env.OKTA_DOMAIN}/oauth2/v1/authorize`,
+  issuer: `https://${process.env.OKTA_DOMAIN}/oauth2/v1`,
   assertClaims: {
     aud: 'api://default',
   },
